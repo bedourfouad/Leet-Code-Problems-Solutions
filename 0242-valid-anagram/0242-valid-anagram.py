37a63344b1e592365c45ddeaac884e9302d452ee
Bedour Fouad <111479119+bedourfouad@users.noreply.github.com>
2024-03-09 22:38:59 +0200

    Time: 40 ms (92.19%), Space: 17 MB (46.08%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_freq = {}
+        t_freq = {}
+        for char in s:
+            s_freq[char] = s_freq.get(char, 0) + 1
+        for char in t:
+            t_freq[char] = t_freq.get(char, 0) + 1
+
+        return s_freq == t_freq
