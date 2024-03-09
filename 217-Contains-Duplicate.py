f67e2f6a9e22593a7320a474ff448d0ab991fba7
Bedour Fouad <111479119+bedourfouad@users.noreply.github.com>
2024-03-09 17:16:03 +0200

    Delete 217-Contains-Duplicate.py

--- 217-Contains-Duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        hset = set()
-        for idx in nums:
-            if idx in hset:
-                return True
-            else:
-                hset.add(idx)
