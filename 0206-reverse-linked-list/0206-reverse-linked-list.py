577a174476005158f388751116ba5c2aa3a7539b
Bedour Fouad <111479119+bedourfouad@users.noreply.github.com>
2024-04-15 21:04:49 +0200

    Time: 37 ms (58.14%), Space: 17.7 MB (34.08%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -15,11 +15,4 @@ class Solution:
         
 
 
-#         prev, curr = None, head
 
-#         while curr:
-#             temp = curr.next
-#             curr.next = prev
-#             prev = curr
-#             curr = temp
-#         return prev
